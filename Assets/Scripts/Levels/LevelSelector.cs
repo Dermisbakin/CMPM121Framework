c08a66471c9f102f7f0b1c116135efc17297cdbf
Saurab S <ajay.v.v.743@gmail.com>
2026-04-28 00:28:03 -0700

    Added speed and damage

--- Assets/Scripts/Levels/LevelSelector.cs
+++ Assets/Scripts/Levels/LevelSelector.cs
@@ -16,6 +16,8 @@ public class Spawn
     public string enemy { get; set; }
     public string count { get; set; }
     public string hp { get; set; }
+    public string speed { get; set; }
+    public string damage { get; set; }
     public string delay { get; set; }
     public int[] sequence { get; set; }
     public string location { get; set; }
