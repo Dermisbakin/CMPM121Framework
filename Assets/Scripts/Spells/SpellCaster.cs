28892ccfa8a2ef9081a7d8a6362b443d7ce68560
Saurab S <ajay.v.v.743@gmail.com>
2026-05-12 20:39:21 -0700

    update spellcaster to support multiple spells w equip and drop

--- Assets/Scripts/Spells/SpellCaster.cs
+++ Assets/Scripts/Spells/SpellCaster.cs
@@ -9,7 +9,13 @@ public class SpellCaster
     public int mana_reg;
     public int power;
     public Hittable.Team team;
+
+    // let player have up to 4 spells
+    public List<Spell> spells;
+    public int activeSpellIndex;
+
     public Spell spell;
+
 
     public IEnumerator ManaRegeneration()
     {
@@ -28,17 +34,59 @@ public class SpellCaster
         this.mana_reg = mana_reg;
         this.power = 10;
         this.team = team;
-        spell = new SpellBuilder().Build(this);
+        this.spells = new List<Spell>();
+        this.activeSpellIndex = 0;
+
+        Spell starter = new SpellBuilder().Seed(this).Build();
+        spells.Add(starter);
     }
 
     public IEnumerator Cast(Vector3 where, Vector3 target)
-    {        
-        if (mana >= spell.GetManaCost() && spell.IsReady())
+    {
+        Spell s = spell;
+        if (s != null && mana >= s.GetManaCost() && s.IsReady())
         {
-            mana -= spell.GetManaCost();
-            yield return spell.Cast(where, target, team);
+            mana -= s.GetManaCost();
+            yield return s.Cast(where, target, team);
         }
         yield break;
     }
 
+    public bool EquipSpell(Spell newSpell)
+    {
+        if (spells.Count < 4)
+        {
+            spells.Add(newSpell);
+            return true;
+        }
+        return false;
+    }
+
+    public void DropSpell(int index)
+    {
+        if (index >= 0 && index < spells.Count)
+        {
+            spells.RemoveAt(index);
+            if (activeSpellIndex >= spells.Count)
+                activeSpellIndex = spells.Count - 1;
+            if (activeSpellIndex < 0)
+                activeSpellIndex = 0;
+        }
+    }
+
+    public void NextSpell()
+    {
+        if (spells.Count > 0)
+            activeSpellIndex = (activeSpellIndex + 1) % spells.Count;
+    }
+
+    public void PrevSpell()
+    {
+        if (spells.Count > 0)
+        {
+            activeSpellIndex--;
+            if (activeSpellIndex < 0)
+                activeSpellIndex = spells.Count - 1;
+        }
+    }
 }
